d1deed3b11716b959a81d948e0d629a49c19c922
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2023-05-13 20:33:56 +0530

    Create 6366. Number of Senior Citizens.java
    6366. Number of Senior Citizens
    Biweekly Contest 104

---
+++ Leetcode/6366. Number of Senior Citizens.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count=0;
+        for(String str : details){
+            if( Integer.valueOf(str.substring(11,13)) > 60){count++;}
+      }
+      return count;
+    }
+}
